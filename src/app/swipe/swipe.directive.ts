c14ea75fda2b862d049cddf464fcf351abc985e3
Arno Teigseth <arnotixe@gmail.com>
2021-06-19 16:14:45 -0500

    added mouse support too, and a comment

--- src/app/swipe/swipe.directive.ts
+++ src/app/swipe/swipe.directive.ts
@@ -13,7 +13,7 @@ export class SwipeDirective implements AfterViewInit {
   /** Timestamp at touchstart */
   time = 0;
 
-  @Output() swipeLeft: EventEmitter<any>;
+  @Output() swipeLeft: EventEmitter<any>; // if writing this code by hand, double check you import EventEmitter from '@angular/core' and not from 'events' or you wil get the error "Type EventEmitter is not generic"
   @Output() swipeRight: EventEmitter<any>;
   // @Output() swipeUp: EventEmitter<any>;
   // @Output() swipeDown: EventEmitter<any>;
@@ -34,6 +34,14 @@ export class SwipeDirective implements AfterViewInit {
     /** Listen for touchend event on element directive is attached to */
     this.renderer.listen(this.elRef.nativeElement, 'touchend', (event: TouchEvent) => {
       this.handleTouchMove(event);
+    });
+
+    this.renderer.listen(this.elRef.nativeElement, 'mousedown', (event: MouseEvent) => {
+      this.handleMouseStart(event);
+    });
+    
+    this.renderer.listen(this.elRef.nativeElement, 'mousemove', (event: MouseEvent) => {
+      this.handleMouseMove(event);
     });
   }
 
@@ -83,5 +91,55 @@ export class SwipeDirective implements AfterViewInit {
     // Reset values.
     this.xDown = null;
     this.yDown = null;
+  }
+  
+  private handleMouseStart(event: MouseEvent) {
+    this.xDown = event.pageX; // unsure if clientX, layerX, pageX, movementX. Not screenX anyways
+    this.yDown = event.pageY;
+    this.time = event.timeStamp;
+  }
+
+  handleMouseMove(event: MouseEvent) {
+    if ( ! this.xDown || ! this.yDown ) {
+      return;
+    }
+
+    /** @see https://stackblitz.com/edit/angular-swipe-events-with-hostlistner */
+    
+    const xUp = event.pageX;
+    const yUp = event.pageY;
+
+
+    const xDiff = this.xDown - xUp;
+    const yDiff = this.yDown - yUp;
+    const timeDiff = event.timeStamp - this.time;
+
+    // simulate a swipe -> less than 500 ms and more than 60 px
+    if (timeDiff < 500) {
+      // touch movement lasted less than 500 ms
+      if (Math.abs(xDiff) > 60) {
+        // delta x is at least 60 pixels
+        if (xDiff > 0) {
+          this.swipeRight.emit(event);
+        } else {
+          this.swipeLeft.emit(event);
+        }
+      }
+
+      /*if (Math.abs(yDiff) > 60) {
+        // delta y is at least 60 pixels
+        if (yDiff > 0) {
+          this.swipeDown.emit(event);
+        } else {
+          this.swipeUp.emit(event);
+        }
+      }*/
+    }
+
+    // Reset values if all mouse buttons up
+    if (event.buttons === 0) {
+      this.xDown = null;
+      this.yDown = null;
+    }
   }
 }
